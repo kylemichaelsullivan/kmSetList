ee35cd33711cf38fc0c639d32485921ca81b7e09
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2024-10-07 00:04:06 -0400

    FIX: ResetSetlist Background (hover)

--- src/components/modes/Performing/ResetSetlist.tsx
+++ src/components/modes/Performing/ResetSetlist.tsx
@@ -9,7 +9,7 @@ function ResetSetlist() {
   return (
     <button
       type="button"
-      className="ResetSetlist flex justify-center items-center border border-current rounded-full ring-blue-500 font-bold text-xxxl w-8 h-8 transition-colors duration-300 hover:bg-gray-400 hover:text-black hover:ring"
+      className="ResetSetlist flex justify-center items-center border border-current rounded-full ring-blue-500 font-bold text-xxxl w-8 h-8 transition-colors duration-300 hover:bg-white hover:text-black hover:ring"
       title="Reset Setlist"
       onClick={resetSetlist}
     >
