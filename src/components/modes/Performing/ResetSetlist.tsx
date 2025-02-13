1889b413bc2dda5aeacf3159854bd3305caf6743
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-02-13 01:49:24 -0500

    FIX: ResetSetlist

--- src/components/modes/Performing/ResetSetlist.tsx
+++ src/components/modes/Performing/ResetSetlist.tsx
@@ -1,21 +1,35 @@
+import { forwardRef } from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRotate } from '@fortawesome/free-solid-svg-icons';
 
 import { useSetlist } from '@/context/setlist';
 
-function RestoreSetlist() {
-	const { restoreSetlist } = useSetlist();
+type ResetSetlistProps = {
+	onReset: () => void;
+};
 
-	return (
-		<button
-			type='button'
-			className='RestoreSetlist text-xxxl flex h-8 w-8 items-center justify-center rounded-full border border-current font-bold ring-blue-500 transition-colors duration-300 hover:bg-white hover:text-black hover:ring'
-			title='Reset Setlist'
-			onClick={restoreSetlist}
-		>
-			<FontAwesomeIcon icon={faRotate} />
-		</button>
-	);
-}
+const ResetSetlist = forwardRef<HTMLButtonElement, ResetSetlistProps>(
+	({ onReset }, ref) => {
+		const { restoreSetlist } = useSetlist();
 
-export default RestoreSetlist;
+		const handleClick = () => {
+			restoreSetlist();
+			onReset();
+		};
+
+		return (
+			<button
+				type='button'
+				className='RestoreSetlist text-xxxl flex w-8 h-8 items-center justify-center rounded-full border border-current font-bold ring-blue-500 transition-colors duration-300 hover:bg-white hover:text-black hover:ring focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 print:hidden'
+				title='Reset Setlist'
+				ref={ref}
+				onClick={handleClick}
+			>
+				<FontAwesomeIcon icon={faRotate} />
+			</button>
+		);
+	},
+);
+
+export default ResetSetlist;
