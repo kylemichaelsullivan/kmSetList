8362c0610fd084c9a22c021c663c7baf53cf0cf7
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-03-06 21:16:49 -0500

    UPDATE: AddSong Functionality

--- src/components/appModes/Editing/AddSongButton.tsx
+++ src/components/appModes/Editing/AddSongButton.tsx
@@ -1,24 +1,51 @@
+import type { RefObject } from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faThumbTack } from '@fortawesome/free-solid-svg-icons';
 
 type AddSongButtonProps = {
 	isAdding: boolean;
+	toggleIsAdding: () => void;
 	handleAddSong: () => void;
+	addSongSongRef: RefObject<HTMLInputElement>;
 };
 
-function AddSongButton({ isAdding, handleAddSong }: AddSongButtonProps) {
+function AddSongButton({
+	isAdding,
+	toggleIsAdding,
+	handleAddSong,
+	addSongSongRef,
+}: AddSongButtonProps) {
+	const handleClick = async () => {
+		if (isAdding) {
+			toggleIsAdding();
+			return;
+		}
+		handleAddSong();
+
+		// ensure ref/input exist after React updates the DOM
+		await new Promise((resolve) => setTimeout(resolve, 1));
+		if (addSongSongRef.current) {
+			addSongSongRef.current.focus();
+		}
+	};
+
 	return (
-		<button
-			type='button'
-			className='AddSongButton text-xxxl flex w-8 h-8 items-center justify-center rounded-full border border-current font-bold ring-blue-500 transition-colors duration-300 hover:bg-black hover:text-white hover:ring'
-			title='Add Song to Catalog'
-			onClick={handleAddSong}
-		>
-			<FontAwesomeIcon
-				icon={!isAdding ? faPlus : faThumbTack}
-				className={isAdding ? 'rotate-180' : ''}
-			/>
-		</button>
+		<div className='AddSongButton flex gap-2 items-center'>
+			<span className='tracking-wide'>{isAdding ? 'Add' : 'New'}</span>
+			<button
+				type='button'
+				className='AddSongButton text-xxxl flex w-8 h-8 items-center justify-center rounded-full border border-current font-bold ring-blue-500 transition-colors duration-300 hover:bg-black hover:text-white hover:ring'
+				title='Add Song to Catalog'
+				onClick={handleClick}
+			>
+				<FontAwesomeIcon
+					icon={!isAdding ? faPlus : faThumbTack}
+					className={isAdding ? 'rotate-180' : ''}
+				/>
+			</button>
+			<span>Song</span>
+		</div>
 	);
 }
 
--- src/components/appModes/Editing/AddSongFields.tsx
+++ src/components/appModes/Editing/AddSongFields.tsx
@@ -1,10 +1,26 @@
-function AddSongFields() {
+import { useState, type RefObject } from 'react';
+
+import { BPM } from '@/lookups';
+
+type AddSongFieldsProps = {
+	isAdding: boolean;
+	addSongSongRef: RefObject<HTMLInputElement>;
+};
+
+function AddSongFields({ isAdding, addSongSongRef }: AddSongFieldsProps) {
+	const [songName, setSongName] = useState('');
+
 	return (
-		<div className='AddSongFields flex flex-col gap-2 w-full group'>
+		<div
+			className={`AddSongFields ${isAdding ? 'flex' : 'hidden'} flex-col gap-2 w-full group`}
+		>
 			<input
 				type='text'
 				className='border border-current text-black w-full px-4 py-2 shadow-lg ring-blue-500 group-hover:ring'
+				value={songName}
+				onChange={(e) => setSongName(e.target.value)}
 				placeholder='Song'
+				ref={addSongSongRef}
 			/>
 
 			<input
@@ -17,6 +33,12 @@ function AddSongFields() {
 				type='number'
 				className='border border-current text-black w-full px-4 py-2 shadow-lg ring-blue-500 group-hover:ring'
 				placeholder='Tempo'
+				// onChange={handleChange}
+				// className='Tempo flex-1 rounded-md border border-current px-4 py-2 shadow-lg ring-blue-500 group-hover:ring'
+				min={BPM.min}
+				max={BPM.max}
+				// aria-label={`Tempo for ${songName}`}
+				// aria-invalid={!isValidBpm(bpm)}
 			/>
 		</div>
 	);
