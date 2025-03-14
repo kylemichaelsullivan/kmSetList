bdfb549c07533f88aa006b7e2de058d4cf1f7830
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-03-14 19:22:30 -0400

    UPDATE: Focus <textarea /> when Modal Opens, Close Modal via Escape & Click Outside

--- src/components/appModes/Playing/Modal.tsx
+++ src/components/appModes/Playing/Modal.tsx
@@ -1,3 +1,5 @@
+import { useEffect, type RefObject } from 'react';
+
 import CloseModal from './CloseModal';
 
 import type { Song } from '@/types';
@@ -5,7 +7,7 @@ import type { Song } from '@/types';
 type ModalProps = {
 	song: Song | null;
 	closeModal: () => void;
-	modalNotesRef: React.RefObject<HTMLTextAreaElement>;
+	modalNotesRef: RefObject<HTMLTextAreaElement>;
 };
 
 function Modal({ song, closeModal, modalNotesRef }: ModalProps) {
@@ -13,22 +15,50 @@ function Modal({ song, closeModal, modalNotesRef }: ModalProps) {
 		return;
 	}
 
+	const handleKeyDown = (e: KeyboardEvent) => {
+		if (e.key === 'Escape') {
+			closeModal();
+		}
+	};
+
+	const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
+		if (e.currentTarget === e.target) {
+			closeModal();
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener('keydown', handleKeyDown);
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+		};
+	}, []);
+
+	useEffect(() => {
+		modalNotesRef.current?.focus();
+	}, [modalNotesRef]);
+
 	return (
-		<div className='Modal absolute-center flex flex-col gap-2 bg-blue-300 text-black p-4'>
-			<CloseModal closeModal={closeModal} />
+		<div
+			className='ModalBackdrop absolute-center w-full h-full'
+			onClick={handleBackdropClick}
+		>
+			<div className='Modal absolute-center flex flex-col gap-2 bg-blue-300 text-black p-4'>
+				<CloseModal closeModal={closeModal} />
 
-			<div className='flex flex-col justify-center items-center gap-4 sm:flex-row'>
-				<p>{song.name}</p>
-				<p>{song.songKey}</p>
-				<p>{song.bpm}</p>
+				<div className='flex flex-col justify-center items-center gap-4 sm:flex-row'>
+					<p>{song.name}</p>
+					<p>{song.songKey}</p>
+					<p>{song.bpm}</p>
+				</div>
+
+				<textarea
+					className='w-full px-4 py-2'
+					name='modalNotes'
+					placeholder={`Notes for ${song.name}`}
+					ref={modalNotesRef}
+				></textarea>
 			</div>
-
-			<textarea
-				className='w-full px-4 py-2'
-				name='modalNotes'
-				placeholder={`Notes for ${song.name}`}
-				ref={modalNotesRef}
-			></textarea>
 		</div>
 	);
 }
